8442716da8dd55abc046985e9336297d746132a5
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-11 20:07:16 +0545

    Correct naming convention

--- src/weekdays.cpp
+++ src/weekdays.cpp
@@ -1,11 +1,21 @@
+/*Program to calculate the number of weeks and days you travelled by taking total days as input*/
+
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+const int DAYS_IN_WEEK = 7;
 
 int main(){
     int trvlDays; 
+    system("cls");
+
     cout<< "Enter the number of Days you have travelled."<< endl;
+
     cin >> trvlDays;
-    cout <<"You have travelled " << trvlDays/7 <<" weeks and " << trvlDays%7 << " days." << endl;  
+    system("cls");
+
+    cout <<"You have travelled " << trvlDays/DAYS_IN_WEEK <<" weeks and " << trvlDays%DAYS_IN_WEEK << " days." << endl;  
 
     return 0;
 }
