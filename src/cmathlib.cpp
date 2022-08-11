35ac621fe260047138dfbc43dfb1fe65e07012b9
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-11 20:29:32 +0545

    ADD camthlib.cpp

---
+++ src/cmathlib.cpp
@@ -0,0 +1,20 @@
+/* Exploring CMath library*/
+#include<iostream>
+#include<cmath>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+    int x = (double)M_PI; 
+    system("cls");
+
+    cout << x << endl << (float)x << endl << (int)x << endl; 
+    /*Casting doesnot work since x is defined to have integer value */
+
+    double y = M_PI;
+
+    cout <<endl << y << endl << (float)y << endl << (int)y << endl; 
+    /* Casting works since x is defined to have double value that can be converted into other datatype equivalent */
+    
+    return 0;
+}
