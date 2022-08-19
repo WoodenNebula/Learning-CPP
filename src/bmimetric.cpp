2b3aa031f9b9c440f397e279613c2d3930f46795
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-19 21:00:28 +0545

    MAKE bmimetric.cpp compatible with metric and imperial system

--- src/bmimetric.cpp
+++ src/bmimetric.cpp
@@ -10,20 +10,44 @@ Where weight is in kilograms and height is in meters.*/
 
 #include <iostream>
 #include <cmath>
+#include <stdlib.h>
 
 using namespace std;
 
+/* 1 pound is  0.453592  kilograms and 1 inch is 0.0254  meters */
+const double M_WEIGHT = 0.453592;   //conversion factor of Weight. # -> KG
+const double M_HEIGHT = 0.0254;     //conversion factor of Height. " -> m
+
+float bmiCalc(float , float);
+
+
 int main(){
+    char x;
     double height, weight;
-    cout << "Please enter weight in kilograms: ";
+    float bmi;
+
+    cout << "Press 'm' for Metric System (KG, Meters) OR 'i' for Imperial System (Pound, Inch)" << endl;
+    cin >> x;
+    system("cls");
+
+    cout << "Please enter WEIGHT: ";
     cin >> weight;
-    cout << "\nPlease enter height in meters: ";
+    cout << "\nPlease enter HEIGHT: ";
     cin >> height; 
 
-    float bmi;
-    bmi = (float) weight/ pow(height, 2) ;
+    if(x == 'I' || x == 'i'){       //Conversion of Imperial to Metric System
+        weight *= M_WEIGHT;
+        height *= M_HEIGHT;
+    }
+        
+    bmi = bmiCalc(weight , height); 
 
     cout.precision(4);      //Changes Precision of the values in cout below this to 4th digit
     cout << "BMI = " << bmi <<endl;
     return 0;
+}
+
+float bmiCalc(float weight, float height){
+    float bmi = (float) weight/ pow(height, 2) ;
+    return bmi;
 }
