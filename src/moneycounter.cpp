e0692a0e947f473c5f4e2df69076135828c09d9c
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-19 19:29:29 +0545

    ADD moneycounter.cpp

---
+++ src/moneycounter.cpp
@@ -0,0 +1,39 @@
+//Get your Quarters, Dimes, Nickels or Pennies(If you have any) and I will convert them into Dollars
+//"25second me paisa double"
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+const int QUARTER_VALUE = 25, NICKEL_VALUE = 5, DIME_VALUE = 10;
+
+
+int main(){
+    int penn, nickNo, dimeNo, quartNo, dollar, cents;    //Types of money
+/*A penny is worth 1 cent
+A nickel is worth 5 cents
+A dime is worth 10 cents
+A quarter is worth 25 cents 
+A Dollar is worth 100 cents*/
+    cout << "No of Coins of Quarters:" <<endl;
+    cin >> quartNo;
+
+    cout << "No of Coins of Dimes:" <<endl;
+    cin >> dimeNo;
+
+    cout << "No of Coins of Nickels:" <<endl;
+    cin>> nickNo;
+
+    cout << "No of Coins of Pennies:" <<endl;
+    cin>> penn;
+
+    cents = (QUARTER_VALUE * quartNo) + (NICKEL_VALUE * nickNo) + (DIME_VALUE * dimeNo) + penn;
+
+    dollar = cents / 100;
+    cents = cents % 100;
+    
+    cout << "You got " << dollar << " $ and " << cents << " cents" << endl;
+    
+    return 0;
+}
