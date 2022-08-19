ab6193c583cae26595ded8e7469cc1f9f27c030f
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-19 21:09:31 +0545

    ADD absoluteval.cpp

---
+++ src/absoluteval.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Give me a number" << endl;
+    cin >> x;
+    cout << abs(x) << endl;
+    return 0;
+}
