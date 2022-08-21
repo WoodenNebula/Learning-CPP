f3417818fd3d477018f5b7bf53901e36a428d6ef
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-21 22:26:38 +0545

    IMPROVE luhn as luhb_array.cpp

---
+++ src/luhn_array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector <int> integerToArray(long long x){
+    vector <int> resultArray;
+    while (true){
+        resultArray.insert(resultArray.begin(), x%10);
+        x /= 10;
+        if(x == 0)
+            return resultArray;
+    }
+}
+
+int main(){
+    long long userCard;
+
+    cout << "Card -> ";
+    cin >> userCard;
+
+    vector <int> actualCard = integerToArray(userCard);
+    
+    for(int i = 0; i < actualCard.size() ; i+= 2){
+        actualCard[i] *= 2;
+        if(actualCard[i] > 9){
+            actualCard[i] = actualCard[i]%10 + actualCard[i]/10;
+        }
+    }
+
+    int sum = 0;
+    for(int i : actualCard){
+        sum += i;
+    }
+    if(sum % 10 == 0){
+        cout <<"\nVALID CARD";
+    }else
+        cout<< "\nINVALID CARD";
+    return 0;
+}
+
+
+
