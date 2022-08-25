923611e519558eec02a56f8f1338a30fc7a3d053
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-25 22:12:41 +0545

    ADD palindrome.cpp

---
+++ src/Leetcode Problems/palindrome.cpp
@@ -0,0 +1,32 @@
+/*Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as forward.
+For example, 121 is a palindrome while 123 is not.*/
+
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h> //For reverse function
+
+
+using namespace std;
+
+int main(){
+    int x;
+    string userInput, revString;
+
+    cout << "Enter an integer" << endl;
+    cin >> x;
+
+    userInput = to_string(x);
+    revString = userInput;
+
+    reverse(revString.begin(), revString.end());
+
+    if(userInput == revString){
+        cout << "Is Palindrome";
+    }
+    else{
+        cout << "Not Palindrome";
+    }
+
+    return 0;
+}
