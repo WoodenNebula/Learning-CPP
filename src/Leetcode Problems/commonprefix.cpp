a7288a2613694ef60e441e110d7a3653784bc4d3
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-26 22:55:15 +0545

    ADD commonprefix.cpp -> String Arrays

---
+++ src/Leetcode Problems/commonprefix.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main (){
+    vector<string> strs;
+    strs = {"flower","flow","flight"};
+        
+    cout << endl;
+    cout << strs.at(1);
+
+
+    return 0;
+}
