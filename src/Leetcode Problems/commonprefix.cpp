357b6c2229a7f0a96a37bb86fc0705e10f6ec9d6
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-27 19:19:20 +0545

    ADD commonprefix.cpp

--- src/Leetcode Problems/commonprefix.cpp
+++ src/Leetcode Problems/commonprefix.cpp
@@ -1,4 +1,7 @@
+//Problem 14 of Leetcode (Semi-Solved)
 
+/* a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "". */
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,11 +10,15 @@ using namespace std;
 
 int main (){
     vector<string> strs;
-    strs = {"flower","flow","flight"};
-        
-    cout << endl;
-    cout << strs.at(1);
-
-
+    strs = {"flower","flow","flown"};
+    int i = 0;
+    if(strs.at(0).at(0) == strs.at(1).at(0) && strs.at(1).at(0) == strs.at(2).at(0)){
+        do{
+            cout << strs.at(0).at(i);
+            if(i >= strs.size()){
+                break;}
+            i++;
+        }while(strs.at(0).at(i) == strs.at(1).at(i) && strs.at(1).at(i) == strs.at(2).at(i));
+    }else cout << "";
     return 0;
 }
