8267f228419554813e7326c6f3ce3b93a7c7ae9b
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2022-08-29 18:40:43 +0545

    USE constructors in classesOne.cpp

--- src/classesOne.cpp
+++ src/classesOne.cpp
@@ -12,15 +12,20 @@ private:
     string m_name;
     int m_age, m_sal;
     
-public:
+public:   
     User(){     //THIS IS A CONSTRUCTOR THAT WILL INITIALIZE DEFAULT VALUES WHEN THE CLASS IS INSTANTIATED
         m_isAlive = false;
         m_name = "[NAME]";
         m_age = 0;
         m_sal = 0;
+        cout << "Created User" << endl;
     }
     
-    void userDetails(bool isAlive , const char* name, int age, int sal){
+    ~User(){        //THIS IS A DESTRUCTOR THAT WILL DESTROY(uninitialize) THE INSTANCE OF THE CLASS
+    
+    }
+
+    User(bool isAlive , const char* name, int age, int sal){ // THIS IS ALSO A CONSTRUCTOR :dead:
         m_isAlive = isAlive;
         m_name = name;
         m_age = age;
@@ -37,17 +42,17 @@ public:
 };
 
 
-int main(){
-    User userOne, userTwo, userThree;
-    
-    userOne.userDetails(true, "Somin", 17, 2500);
+int main(){    
+
+    User userOne(true, "Somin", 17, 2500);
     userOne.showDetails();
     cout<< "<===================================>" << endl;
 
+    User userTwo;
     userTwo.showDetails();
     cout<< "<===================================>" << endl;
 
-    userThree.userDetails(true, "Surab", 17, 1000);
+    User userThree(true, "Surab", 17, 1000);
     userThree.showDetails();
     cout<< "<===================================>" << endl;
 
