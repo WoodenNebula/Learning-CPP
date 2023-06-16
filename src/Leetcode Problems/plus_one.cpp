441df5e2a04c905249600be4b319a4115095ce1f
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2023-06-16 20:56:01 +0545

    Complete plus one: problem 66 of Leetcode

--- src/Leetcode Problems/plus_one.cpp
+++ src/Leetcode Problems/plus_one.cpp
@@ -1,5 +1,4 @@
-//Problem 66 of Leetcode
-
+//✔✔ Problem 66 of Leetcode
 
 /*A large integer is given represented as an integer array digits, 
 where each digits[i] is the ith digit of the integer.
@@ -13,14 +12,61 @@ Increment the large integer by one and return the resulting array of digits.*/
 class Solution
 {
 public:
-    std::vector<int> PlusOne(std::vector<int>& digits)
-    {
+    std::vector<int> _digits;
 
+public:
+    Solution(const std::vector<int>& digits)
+        :_digits(digits)
+    {
+        _digits = PlusOne(_digits);
+        std::cout << "Digits after +1";
+        PrintDigits();
+    }
+
+
+    std::vector<int> PlusOne(std::vector<int> digits) 
+    {
+        //pointer to the last element
+        std::vector<int>::iterator itr = (digits.end() - 1);
+        
+        (*itr)++;
+
+        while(*itr > 9)
+        {
+            //sets current digit to 0 (10) carry 1
+            (*itr) = 0;
+            //check if the carry is on first element
+            if(itr == digits.begin())
+            {   
+                //add 1 to the first element
+                digits.insert(digits.begin(), 1);
+                continue;
+            }
+            //set the pointer to 1 digit left
+            itr = itr - 1;
+            //increase the value
+            (*itr)++;
+        }
+
+        return digits;
+    }
+
+    void PrintDigits() const
+    {
+        std::cout << '\n';
+        for(const auto& x: _digits)
+        {
+            std::cout << x;
+        }
+        std::cout << "\n";
     }
 };
 
 int main()
 {
-    
+    Solution s({9});
+    Solution s1({9,8,9});
+    Solution s2({9,9,9,9,9,9,9,9,9,9});
+
     return 0;
 }
